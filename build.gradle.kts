7fb29c023a098a62831c2d844ca1817de3b1c64b
Eduardo Fonseca <edrd.fons@gmail.com>
2025-05-01 19:18:45 -0300

    Update inertia4j version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,18 +4,16 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
-group = "dev.edrd"
-version = "unspecified"
+group = "io.github.inertia4j"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.github.inertia4j:inertia4j-spring") // Review dependency once package is published.
-
+    implementation("io.github.inertia4j:inertia4j-spring:1.0.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
